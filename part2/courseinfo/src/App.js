7c72057a58216d3b4fa015f3bc973d2ce0edd3ce
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-14 21:21:20 +0100

    step 6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,66 @@
+
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part, index) => <Part key={index} part={part["name"]} exercise={part["exercises"]} />)}
+    </>
+  )
+
+}
+const Part = ({ part, exercise }) => {
+  return (
+    <p>
+      {part} {exercise}
+    </p>
+  )
+}
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+export default App
