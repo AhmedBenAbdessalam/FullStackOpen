79a27869ab410193825490bd4713a0b72fe5b393
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-15 18:33:16 +0100

    step 2

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -10,22 +10,26 @@ const Filter = ({ searchTerm, handleSearch }) => {
   )
 }
 
-const Display = ({ displayCountries }) => {
+const Details = ({ country }) => {
+  return (
+    <>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital[0]}</p>
+      <p>area {country.area}</p>
+      <h2>Languages:</h2>
+      <ul>{Object.values(country.languages).map((lang, index) => <li key={index}>{lang}</li>)}</ul>
+      <img src={country.flags.png} alt={`flag of ${country.name.common}`}></img>
+    </>
+  )
+}
+
+const Display = ({ displayCountries, setDisplayCountries }) => {
   if (displayCountries.length > 10) {
     return <p>{'Too many matches, specify another filter'}</p>
   }
   else if (displayCountries.length === 1) {
     const country = displayCountries[0]
-    return (
-      <>
-        <h1>{country.name.common}</h1>
-        <p>capital {country.capital[0]}</p>
-        <p>area {country.area}</p>
-        <h2>Languages:</h2>
-        <ul>{Object.values(country.languages).map((lang, index) => <li key={index}>{lang}</li>)}</ul>
-        <img src={country.flags.png} alt={`flag of ${country.name.common}`}></img>
-      </>
-    )
+    return <Details country={country} />
   }
   else if (displayCountries.length === 0) {
     return <p>{'Country doesnt exist'}</p>
@@ -33,7 +37,9 @@ const Display = ({ displayCountries }) => {
   else {
     return (
       <>
-        {displayCountries.map((country, index) => <p key={index}>{country.name.common}</p>)}
+        {displayCountries.map((country, index) => <p key={index}>{country.name.common} <button onClick={() => {
+          setDisplayCountries([country])
+        }} >show</button> </p>)}
       </>
     )
   }
@@ -60,7 +66,6 @@ const App = () => {
 
   const filterCountries = () => {
     const newDisplayCountries = countries.filter(country => country.name.common.toLowerCase().includes(searchTerm.toLowerCase()))
-    console.log(newDisplayCountries)
     setDisplayCountries(newDisplayCountries)
   }
   useEffect(filterCountries, [searchTerm, countries])
@@ -68,7 +73,7 @@ const App = () => {
   return (
     <div>
       <Filter searchTerm={searchTerm} handleSearch={handleSearch} />
-      <Display displayCountries={displayCountries} />
+      <Display displayCountries={displayCountries} setDisplayCountries={setDisplayCountries} />
     </div>
   );
 }
