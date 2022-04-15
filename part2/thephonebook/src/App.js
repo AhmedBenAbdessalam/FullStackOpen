26bc125e07e165310ee91bdf358c34c97e475716
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-15 17:33:12 +0100

    step 6

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const SearchFilter = ({ searchTerm, setSearchTerm }) => {
   const HandleSearchChange = (e) => {
@@ -48,12 +49,16 @@ const DisplayNumbers = ({ persons, searchTerm }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(res => res.data)
+      .then((data) => {
+        setPersons(data)
+      })
+
+  }, [])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
