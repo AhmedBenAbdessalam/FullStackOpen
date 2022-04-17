049be8624f56036f3863714064ca5fbd967f37f9
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-18 00:20:52 +0100

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,12 +1,26 @@
 const dummy = (blogs) => {
   return 1
 }
+
 const totalLikes = (blogs) => {
   const nbLikes = blogs.reduce((sum,blog) => sum + blog.likes,0)
   return nbLikes
 }
 
+const favoriteBlog =(blogs) => {
+  const popularBlog = blogs.reduce((popular,blog) => {
+    console.log(popular, blog)
+    return (popular.likes > blog.likes) ? popular : blog
+  },{})
+
+  if (!popularBlog.title)
+    return 0
+  else
+    return { title: popularBlog.title, author: popularBlog.author, likes:popularBlog.likes }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
