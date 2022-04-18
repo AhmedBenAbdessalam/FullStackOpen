bd60778ad41ee89a7c00f0fed6ba608ff55c5457
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-18 22:05:54 +0100

    4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'cool blog 1',
+    author: 'cool author 1',
+    url: 'cool url 1',
+    likes: 7
+  },
+  {
+    title: 'cool blog 2',
+    author: 'cool author 2',
+    url: 'cool url 2',
+    likes: 3
+  },
+  {
+    title: 'cool blog 3',
+    author: 'cool author 2',
+    url: 'cool url 3',
+    likes: 1
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body.length).toBe(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
