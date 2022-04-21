eee8af16399c156253882ddf3f8e664beddeed85
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-21 23:59:27 +0100

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,22 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import {React, useState} from 'react'
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  return(
+  <div style={blogStyle}>
+    {blog.title} {blog.author} <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+    <div style={showWhenVisible}>
+      <div>{blog.url}</div>
+      <div>{blog.likes} likes <button >like</button></div>
+      <div>added by {blog.user.name}</div>
+    </div>
   </div>  
-)
-
+)}
 export default Blog
