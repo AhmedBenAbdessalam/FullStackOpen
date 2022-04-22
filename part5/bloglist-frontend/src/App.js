03976e04e04b443fef81e21b188a2f79610def3d
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-22 01:07:45 +0100

    5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -106,7 +106,8 @@ const App = () => {
           setNotification={setNotification}
         />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a,b) => b.likes - a.likes)
+      .map(blog =>
         <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
     </div>
