29d710acdb1c1faaecd5e433c11e70d6086f9aa7
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-22 09:14:27 +0100

    5.12

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -11,15 +11,15 @@ const Blog = ({ blog, handleLike, handleRemove, name}) => {
   }
   const showWhenVisible = { display: visible ? '' : 'none' }
   return(
-  <div style={blogStyle}>
-    {blog.title} {blog.author} <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
-    <div style={showWhenVisible}>
-      <div>{blog.url}</div>
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
         <div>{blog.likes} likes <button onClick={() => { handleLike(blog)}} >like</button></div>
-      <div>added by {blog.user.name}</div>
+        <div>added by {blog.user.name}</div>
         {name === blog.user.name ? <button onClick={() => { handleRemove(blog) }}>remove</button> : null}   
+      </div>
     </div>
-  </div>  
 )}
 
 Blog.propTypes = {
