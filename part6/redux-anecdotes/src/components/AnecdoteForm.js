20e67d0db39b8a6dff69ff04770bdb8925bf6116
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-24 00:30:27 +0100

    6.14

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,22 +1,30 @@
 import { useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { removeNotification, setNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../Services/anecdotes'
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
-  const createAnecdote = (e) => {
+  const createAnecdote = async (e) => {
     e.preventDefault()
-    dispatch(addAnecdote(e.target.anecdote.value))
-    e.target.anecdote.value = ''
-    dispatch(setNotification('you added a new anecdote'))
-    setTimeout(() => {
-      dispatch(removeNotification())
-    }, 5000);
+    const content = e.target.anecdote.value
+    if (content)
+    {
+      const newAnecdote = await anecdoteService.createNew(content)
+      console.log(newAnecdote)
+      dispatch(addAnecdote(newAnecdote))
+      e.target.anecdote.value = ''
+      dispatch(setNotification('you added a new anecdote'))
+      setTimeout(() => {
+        dispatch(removeNotification())
+      }, 5000);
+    }
+    
   }
   return(
     <>
       <h2>create new</h2>
       <form onSubmit={createAnecdote}>
-        <div><input name='anecdote' /></div>
+        <div><input name='anecdote' required /></div>
         <button type='submit'>create</button>
       </form>
       </>
