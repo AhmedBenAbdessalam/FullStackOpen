6101c7921cb420e144354e2ee52bc8ea4c1b42f2
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-27 02:12:11 +0100

    8.4

--- part8/library-app/index.js
+++ part8/library-app/index.js
@@ -103,7 +103,7 @@ type Author {
 type Query {
   bookCount: Int!
   authorCount: Int!
-  allBooks: [Book!]!
+  allBooks(author:String): [Book!]!
   allAuthors: [Author!]!
 }
 `
@@ -112,7 +112,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author)
+        return books
+      else
+        return books.filter(book => book.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {
